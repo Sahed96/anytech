db1fb5e0e9892f0b9ef7599afcf7c3fb2fcc8a91
Sahed96 <ninja.sahedkhan@gmail.com>
2025-02-02 02:06:32 +0600

    philosophy section done

--- src/Components/Philosphy.jsx
+++ src/Components/Philosphy.jsx
@@ -1,5 +1,8 @@
 import bridge from '../assets/icons/easy-banking/Bridge.svg';
-import hline from '../assets/icons/easy-banking/Line4.svg';
+import hline from '../assets/icons/easy-banking/Line1.svg';
+import hline2 from '../assets/icons/easy-banking/Line4.svg';
+import hline3 from '../assets/icons/easy-banking/Line2.svg';
+import hline4 from '../assets/icons/easy-banking/Line3.svg';
 
 const Philosophy = () => {
   return (
@@ -35,22 +38,92 @@ const Philosophy = () => {
         </div>
 
         {/* Right Card */}
-        <div className="bg-[#e9f6ff] rounded-3xl min-w-[808px] min-h-[400px] relative z-10">
-          {/* Right card content goes here */}
+        <div className="bg-[#e9f6ff] flex justify-center items-start rounded-3xl min-w-[808px] min-h-[400px] relative z-10">
+          <div className="absolute top-32 left-16">
+            <div className="bg-[#fff] mb-2 flex flex-col justify-center items-center rounded-3xl shadow-2xl w-[244px] min-h-[115px]">
+              <div>
+                <p className="text-sm mb-2 mt-2 text-[#2b79da] leading-6 tracking-[2.56px] font-bold">
+                  MECHANISM
+                </p>
+                <h3 className="text-[#0b305b] text-4xl font-semibold">
+                  Blockchain
+                </h3>
+              </div>
+            </div>
+            <div className="w-[144px]">
+              <p className="text-sm font-semibold text-[#9497b5] mt-5">
+                Enhanced security by eliminating intermediaries
+              </p>
+            </div>
+          </div>
+          <div className="absolute bottom-4 right-10 flex items-start gap-4">
+            <div className="bg-[#fff] mb-2 flex justify-center items-center rounded-3xl shadow-2xl w-[244px] min-h-[115px]">
+              <div>
+                <p className="text-sm mb-2 mt-2 text-[#2b79da] leading-6 tracking-[2.56px] font-bold">
+                  RESOURCE
+                </p>
+                <h3 className="text-[#0b305b] text-4xl font-semibold">Data</h3>
+              </div>
+            </div>
+            <div className="w-[144px]">
+              <p className="text-sm font-semibold text-[#9497b5] mt-5">
+                Valuable, actionable insights from mass data sources
+              </p>
+            </div>
+          </div>
+
+          <div className="absolute flex gap-4 top-4 right-10">
+            <div className="bg-[#fff] mb-2 flex flex-col justify-center items-center rounded-3xl shadow-2xl w-[244px] min-h-[115px]">
+              <div>
+                <p className="text-sm mb-2 mt-2 text-[#2b79da] leading-6 tracking-[2.56px] font-bold">
+                  INFRASTRUCTURE
+                </p>
+                <h3 className="text-[#0b305b] text-4xl font-semibold">Cloud</h3>
+              </div>
+            </div>
+            <div className="w-[144px]">
+              <p className="text-sm font-semibold text-[#9497b5] mt-5">
+                Scale resources easily on-demand
+              </p>
+            </div>
+          </div>
         </div>
 
         {/* Bridge SVG (Positioned Under Both Cards) */}
         <div className="absolute inset-0 z-0 -left-[300px] flex justify-center items-center">
-          <img
-            src={bridge}
-            alt="Bridge"
-            className="rotate-90 w-[300px]  "
-          />
+          <img src={bridge} alt="Bridge" className="rotate-90 w-[300px]" />
         </div>
 
         {/* HLine SVG (Centered Within Bridge SVG) */}
         <div className="absolute inset-0 flex -left-[300px] justify-center items-center z-20">
-          <img src={hline} alt="Horizontal Line" className="w-[150px]" />
+          <img
+            src={hline}
+            alt="Horizontal Line"
+            className="h-[130px] rotate-90"
+          />
+        </div>
+        <div className="absolute inset-0 flex left-[730px] justify-center items-center z-20">
+          <img
+            src={hline2}
+            alt="Horizontal Line"
+            className="w-[100px] rotate-90"
+          />
+        </div>
+
+        {/* Arrows under the Blockchain div */}
+        <div className="absolute inset-0 flex -bottom-[184px] left-[380px] justify-center items-center z-10">
+          <img
+            src={hline3}
+            alt="Horizontal Line"
+            className="w-[100px] -rotate-90"
+          />
+        </div>
+        <div className="absolute inset-0  flex -top-[240px] left-[380px] justify-center items-center z-10">
+          <img
+            src={hline4}
+            alt="Horizontal Line"
+            className="w-[100px] -rotate-90"
+          />
         </div>
       </div>
     </div>
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from 'react';
-import { Menu, X, Globe } from 'lucide-react';
-import logo from '../../public/logo.jpg';
+import { Menu, X, Globe, ChevronRight } from 'lucide-react';
+import logo from '../../public/logo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [isTransparent, setIsTransparent] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > lastScrollY) {
-        setIsVisible(false);
+        setIsVisible(false); // Hide navbar when scrolling down
       } else {
-        setIsVisible(true);
+        setIsVisible(true); // Show navbar when scrolling up
       }
+
+      setIsTransparent(window.scrollY < 100); // Adjust transparency based on scroll position
       setLastScrollY(window.scrollY);
     };
 
@@ -23,31 +26,34 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`bg-white shadow-md sticky top-0 left-0 w-full transition-transform duration-300 ${
+      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
         isVisible ? 'translate-y-0' : '-translate-y-full'
+      } ${
+        isTransparent
+          ? 'bg-transparent text-white'
+          : 'bg-white text-black shadow-md'
       }`}
     >
-      <div className="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           {/* Logo */}
-          <div className="w-36">
-            <img src={logo} alt="" />
+          <div className="w-36 ">
+            <img className="bg-white p-2.5 rounded-xl" src={logo} alt="Logo" />
           </div>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-16">
-            <a href="#" className="text-blue-500 hover:underline">
+            <a href="#" className="text-xl hover:underline">
               Solutions
             </a>
-            <a href="#" className="text-blue-500 hover:underline">
+            <a href="#" className="text-xl hover:underline">
               Services
             </a>
-            <a href="#" className="text-blue-500 hover:underline">
+            <a href="#" className="text-xl hover:underline">
               About Us
             </a>
             <a>
-              {' '}
-              <button className="flex items-center space-x-1 border px-3 py-1 rounded-lg text-blue-500">
+              <button className="flex items-center space-x-1 border px-3 py-1 rounded-lg">
                 <Globe size={16} />
                 <span className="flex items-center space-x-1">
                   EN{' '}
@@ -72,10 +78,19 @@ const Navbar = () => {
             </a>
           </div>
 
-          {/* Language & Contact Button */}
+          {/* Contact Button */}
           <div className="hidden md:flex items-center space-x-4">
-            <button className="bg-orange-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-orange-600">
+            <button
+              className={`${
+                isTransparent
+                  ? 'text-white bg-transparent outline-2'
+                  : 'text-black bg-orange-500'
+              } text-white px-6 py-2 rounded-lg text-xl font-semibold shadow-md flex items-center  space-x-1`}
+            >
               Contact Us
+              <div>
+                <ChevronRight />
+              </div>
             </button>
           </div>
 
